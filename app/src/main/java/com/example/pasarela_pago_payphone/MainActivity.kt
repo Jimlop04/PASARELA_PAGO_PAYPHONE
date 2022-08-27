f82d263db14ca3af9ad666044da73189f41fc0b7
Jimlop04 <jimmy.lopez2017@uteq.edu.ec>
2022-08-27 18:36:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pasarela_pago_payphone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pasarela_pago_payphone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
